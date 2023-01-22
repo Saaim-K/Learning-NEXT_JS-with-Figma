6d5f78d5ad9fee7f4c71e04531e6ab530cde3b1e
Saaim-K <saaimkhan221@gmail.com>
2023-01-23 02:16:33 +0500

    Login Page Done

--- Components/background.jsx
+++ Components/background.jsx
@@ -1,11 +1,17 @@
-import Image from "next/image"
-import styles from '@/styles/background.module.css'
+import styles from '@/styles/Background.module.css'
 
 const background = () => {
     return (
-        <>
-            <div className={styles.background}></div>
-        </>
+        <div className={styles.background}>
+            <div className={styles.rectangle}>
+                <div className={styles.pyxis}></div>
+                <div className={styles.logo}></div>
+                <div className={styles.text}>
+                    <p>Advanced Search-Find your smartest<br /> way to the buyer.</p>
+                    <p className={styles.subtext}>Start your company in minutes</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
