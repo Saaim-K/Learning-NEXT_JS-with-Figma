d00247dd6246ddae394de51e2935aef9cfc5cc73
Saaim-K <saaimkhan221@gmail.com>
2023-01-22 18:07:58 +0500

    Background Done

---
+++ Components/background.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+import styles from '@/styles/background.module.css'
+
+const background = () => {
+    return (
+        <>
+            <div className={styles.background}></div>
+        </>
+    )
+}
+
+export default background
